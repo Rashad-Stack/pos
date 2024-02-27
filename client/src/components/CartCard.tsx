c95a2b4037a39a9b6e1ba7e9f36ab4a3f2389b1b
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-02-27 12:15:54 +0600

    Remove delete functionality from CartCard component

--- client/src/components/CartCard.tsx
+++ client/src/components/CartCard.tsx
@@ -1,10 +1,7 @@
-import { REMOVE_FROM_CART } from "@/graphql/mutation";
-import { ALL_CARTS } from "@/graphql/query";
 import { Cart } from "@/types";
-import { useMutation } from "@apollo/client";
-import toast from "react-hot-toast";
-import { HiOutlinePencilSquare, HiOutlineTrash } from "react-icons/hi2";
+import { HiOutlinePencilSquare } from "react-icons/hi2";
 import DecreaseQuantity from "./DecreaseQuantity";
+import DeleteCart from "./DeleteCart";
 import IncreaseQuantity from "./IncreaseQuantity";
 
 type Props = {
@@ -13,28 +10,6 @@ type Props = {
 
 export default function CartCard({ cart }: Props) {
   const { product, quantity, totalPrice, _id } = cart || {};
-
-  const [remove, { loading }] = useMutation(REMOVE_FROM_CART);
-
-  const spinner = (
-    <div className="h-4 w-4 animate-spin rounded-full border-2 border-gray-300 border-t-indigo-600" />
-  );
-
-  function handleRemoveFromCart() {
-    toast.promise(
-      remove({
-        variables: {
-          productId: _id,
-        },
-        refetchQueries: [{ query: ALL_CARTS }],
-      }),
-      {
-        loading: `Removing ${product?.name} from cart`,
-        success: `${product?.name} removed from cart`,
-        error: `Error removing ${product?.name} from cart`,
-      },
-    );
-  }
 
   return (
     <div className="flex items-center gap-2">
@@ -53,13 +28,7 @@ export default function CartCard({ cart }: Props) {
           <p className="text-xs">Total: ${totalPrice}</p>
         </div>
       </div>
-      <button onClick={handleRemoveFromCart} disabled={loading}>
-        {loading ? (
-          spinner
-        ) : (
-          <HiOutlineTrash className="h-6 w-6 text-red-400" />
-        )}
-      </button>
+      <DeleteCart id={_id} />
     </div>
   );
 }
---
+++ client/src/components/DeleteCart.tsx
@@ -0,0 +1,39 @@
+import { REMOVE_FROM_CART } from "@/graphql/mutation";
+import { ALL_CARTS } from "@/graphql/query";
+import { useMutation } from "@apollo/client";
+import toast from "react-hot-toast";
+import { HiOutlineTrash } from "react-icons/hi2";
+
+type Props = {
+  id: string;
+};
+
+export default function DeleteCart({ id }: Props) {
+  const [remove, { loading }] = useMutation(REMOVE_FROM_CART);
+
+  const spinner = (
+    <div className="h-4 w-4 animate-spin rounded-full border-2 border-gray-300 border-t-indigo-600" />
+  );
+
+  function handleRemoveFromCart() {
+    toast.promise(
+      remove({
+        variables: {
+          productId: id,
+        },
+        refetchQueries: [{ query: ALL_CARTS }],
+      }),
+      {
+        loading: `Removing from cart`,
+        success: `Removed from cart`,
+        error: `Error removing from cart`,
+      },
+    );
+  }
+
+  return (
+    <button onClick={handleRemoveFromCart} disabled={loading}>
+      {loading ? spinner : <HiOutlineTrash className="h-6 w-6 text-red-400" />}
+    </button>
+  );
+}
